1ac1c5af2efbd229ec40578c011696ad61bc62fc
Sharath3116 <sharath3192@gmail.com>
2025-04-30 18:34:34 +0530

    Add roboshop directory to main branch

---
+++ roboshop/01-MongoDB.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#colour code
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+    echo -e "Starting the Script $TIMESTAMP" &>> LOGFILE
+VALIDATE (){
+             if [ $1 -ne 0 ]
+            then 
+                echo -e "$2 $R Failed$N"
+                exit 1
+            else
+                echo -e "$2 $G Sucess$N"
+            fi 
+            }
+#checking root user status
+if [ $ID -ne 0 ]
+then
+    echo -e "Please run script with$R root user $N"
+    exit 1
+else
+    echo -e "Your running with $G root user $N"
+fi
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
