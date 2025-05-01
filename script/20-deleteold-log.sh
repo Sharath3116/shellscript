a4aff43570be40b7154cdeca1bd32a1f3aed3e34
Sharath3116 <sharath3192@gmail.com>
2025-05-01 13:08:20 +0530

    script update

--- script/20-deleteold-log.sh
+++ script/20-deleteold-log.sh
@@ -13,11 +13,11 @@ then
     exit 1
 fi
 
-Files_to_delete=$(find $DIRECTORY -type f -mtime +14 -name "*.log")
+Files_to_delete=$(find $DIRECTORY -type f -mtime +200 -name "*.log")
 
 while IFS= read -r line
 do
     echo -e "$R deleting log $line $N"
-    rm -rf $line
+   
 
 done <<< "$Files_to_delete"
