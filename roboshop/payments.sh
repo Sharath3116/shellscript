c0380942c282d077568669e8cab348e29ab6dac2
Sharath3116 <sharath3192@gmail.com>
2025-05-01 23:09:14 +0530

    script update

---
+++ roboshop/payments.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#colour code
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+    echo -e "Starting the Script $TIMESTAMP" &>> $LOGFILE
+VALIDATE (){
+             if [ $1 -ne 0 ]
+            then 
+                echo -e "$2 $R Failed$N"
+                exit 1
+            else
+                echo -e "$2 $G Sucess$N"
+            fi 
+            }
+#checking root user status
+if [ $ID -ne 0 ]
+then
+    echo -e "Please run script with$R root user $N"
+    exit 1
+else
+    echo -e "Your running with $G root user $N"
+fi
+
+dnf install python3.11 gcc python3-devel -y &>> $LOGFILE
+    VALIDATE $? "rabbitmq package install"
