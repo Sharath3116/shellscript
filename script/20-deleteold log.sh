96e6ac6dc94a5d20871493d501b3e478dd62c8fb
Sharath3116 <sharath3192@gmail.com>
2025-05-01 12:22:21 +0530

    script update

---
+++ script/20-deleteold log.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+DIRECTORY="/log/test-log"
+
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+N='\e[0m'
+
+if [ -d $DIRECTORY ]
+then    
+    echo -e "$R Directory does not exist$N"
+else   
+    echo -e "$G Directory exists$N"
+fi
